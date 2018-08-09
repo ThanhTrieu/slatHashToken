da37271d1794819050de2304b7644845ab71683a
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2018-08-09 10:10:46 +0700

    update code

--- example.js
+++ example.js
@@ -3,5 +3,5 @@
 var ssoToken = require("./");
 var token = ssoToken.createToken('192.168.6.161','AES-256-CBC','38754d453834736e4363437964796642505756735a7458396159674448724555');
 console.log(token);
-var deToken = ssoToken.tokenValid(token,'AES-256-CBC','38754d453834736e4363437964796642505756735a7458396159674448724555');
+var deToken = ssoToken.tokenValid(token,'AES-256-CBC','192.168.6.161','38754d453834736e4363437964796642505756735a7458396159674448724555');
 console.log(deToken);
--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 "use strict";
-
 var crypto = require('crypto');
-const SSO_DOMAIN = '192.168.6.161';
-const SSO_KEY = '38754d453834736e4363437964796642505756735a7458396159674448724555';
+
+function str_replace (search, replace, subject, countObj) {
+
+  var i = 0
+  var j = 0
+  var temp = ''
+  var repl = ''
+  var sl = 0
+  var fl = 0
+  var f = [].concat(search)
+  var r = [].concat(replace)
+  var s = subject
+  var ra = Object.prototype.toString.call(r) === '[object Array]'
+  var sa = Object.prototype.toString.call(s) === '[object Array]'
+  s = [].concat(s)
+
+  var $global = (typeof window !== 'undefined' ? window : global)
+  $global.$locutus = $global.$locutus || {}
+  var $locutus = $global.$locutus
+  $locutus.php = $locutus.php || {}
+
+  if (typeof (search) === 'object' && typeof (replace) === 'string') {
+    temp = replace
+    replace = []
+    for (i = 0; i < search.length; i += 1) {
+      replace[i] = temp
+    }
+    temp = ''
+    r = [].concat(replace)
+    ra = Object.prototype.toString.call(r) === '[object Array]'
+  }
+
+  if (typeof countObj !== 'undefined') {
+    countObj.value = 0
+  }
+
+  for (i = 0, sl = s.length; i < sl; i++) {
+    if (s[i] === '') {
+      continue
+    }
+    for (j = 0, fl = f.length; j < fl; j++) {
+      temp = s[i] + ''
+      repl = ra ? (r[j] !== undefined ? r[j] : '') : r[0]
+      s[i] = (temp).split(f[j]).join(repl)
+      if (typeof countObj !== 'undefined') {
+        countObj.value += ((temp.split(f[j])).length - 1)
+      }
+    }
+  }
+  return sa ? s : s[0]
+}
 
 function dateAdd(date, interval, units) {
     var ret = new Date(date); //don't change original date
@@ -44,25 +92,27 @@ function decryptToken (encryptedMessage, encryptionMethod, secret){
     return decryptor.update(encryptedMessage, 'base64', 'utf8') + decryptor.final('utf8');
 }
 
-module.exports.createToken = function(plain_text, encryptionMethod, secret){
+module.exports.createToken = function(product, encryptionMethod, secret){
     if(secret.length > 32){
         secret = secret.substr(0, 32);
     }
     var iv = secret.substr(0, 16);
     var timeLifeToken = getCurrentDate();
-    var textToEncrypt = plain_text + '|' + dateAdd(timeLifeToken,'minute',3);
+    var textToEncrypt = product + '|' + dateAdd(timeLifeToken,'minute',3);
 
-    var encryptor = crypto.createCipheriv(encryptionMethod, secret, iv);
-    return encryptor.update(textToEncrypt, 'utf8', 'base64') + encryptor.final('base64');
+    var encryptor  = crypto.createCipheriv(encryptionMethod, secret, iv);
+    var encryptors = encryptor.update(textToEncrypt, 'utf8', 'base64') + encryptor.final('base64');
+    return str_replace(['+','/'],['-','_'],encryptors);
 }
 
-module.exports.tokenValid = function(encryptedMessage, encryptionMethod, secret){
+module.exports.tokenValid = function(encryptedMessage, encryptionMethod, product, secret){
+    encryptedMessage = str_replace(['-','_'],['+','/'],encryptedMessage);
     var decodeToken = decryptToken(encryptedMessage, encryptionMethod, secret);
     var plainText = decodeToken.substr(0,13).trim();
     var timeToken = decodeToken.substr(14,decodeToken.length).trim();
     var currentTime = Date.parse(new Date());
     var lifeTimeToken  = Date.parse(timeToken);
-    if(lifeTimeToken > currentTime && plainText === SSO_DOMAIN){
+    if(lifeTimeToken > currentTime && plainText === product){
         return true;
     }
     return false;
